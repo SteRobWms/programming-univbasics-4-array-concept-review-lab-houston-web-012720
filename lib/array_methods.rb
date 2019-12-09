30652a73736864e3e717991329f843b07cf1272c
Stephen Williams <stephen.r.williams.87@gmail.com>
2019-12-09 03:43:08 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,5 @@
 def find_element_index(array, value_to_find)
-  if array.index(value_to_find) do
-    return array.index(value_to_find)
-  end
-else return nil
-  end
+  return array.index(value_to_find)
 end
 
 def find_max_value(array)
