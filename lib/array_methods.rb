b55f8fd56a349f8f53cc4370c88bfac898833a82
Stephen Williams <stephen.r.williams.87@gmail.com>
2019-12-09 03:45:49 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,11 +3,11 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  array.sort
-  return array[-1]
+  new_array = array.sort
+  return new_array[-1]
 end
 
 def find_min_value(array)
-  array.sort
-  return array[0]
+  new_array = array.sort
+  return new_array[0]
 end
